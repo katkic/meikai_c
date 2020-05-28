62273b07875fd02293c75af2b44cfc45e2c6a39d
daisaku kato <50142017+katkic@users.noreply.github.com>
2020-05-28 19:18:22 +0900

    add: list0110.c

---
+++ chap01/list0110.c
@@ -0,0 +1,13 @@
+// 二つの変数を初期化して表示
+#include <stdio.h>
+
+int main(void)
+{
+    int vx = 57;
+    int vy = vx + 10;
+
+    printf("vxの値は%dです。\n", vx);
+    printf("vyの値は%dです。\n", vy);
+
+    return 0;
+}
