ebdde033a7df6421520dc7e45a5e982d3aa084e3
daisaku kato <50142017+katkic@users.noreply.github.com>
2020-05-28 19:17:49 +0900

    add: list0108.c

---
+++ chap01/list0108.c
@@ -0,0 +1,15 @@
+// 二つの変数に整数値を格納して表示
+#include <stdio.h>
+
+int main(void)
+{
+    int vx, vy;
+
+    vx = 57;
+    vy = 67;
+
+    printf("vxの値は%dです。\n", vx);
+    printf("vyの値は%dです。\n", vy);
+
+    return 0;
+}
