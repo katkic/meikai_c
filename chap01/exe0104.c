6e8f21501423301d8b2ac9c06ea72e7253355fa7
daisaku kato <50142017+katkic@users.noreply.github.com>
2020-05-28 19:18:37 +0900

    add: exe0104.c

---
+++ chap01/exe0104.c
@@ -0,0 +1,12 @@
+// 演習1-4
+// int型変数の宣言に実数値の初期化子（例えば3.14や5.7など）を与えるとどうなるか。
+#include <stdio.h>
+
+int main(void)
+{
+    int value = 3.14;
+
+    printf("valueの値は%dです。\n", value);
+
+    return 0;
+}
