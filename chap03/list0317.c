bb9b88b132a7efce178fef540391e11b11af966f
daisaku kato <50142017+katkic@users.noreply.github.com>
2020-05-31 12:59:11 +0900

    add: list0317.c

---
+++ chap03/list0317.c
@@ -0,0 +1,24 @@
+// 読み込んだ月の季節を表示
+#include <stdio.h>
+
+int main(void)
+{
+    int month;
+
+    printf("何月ですか？ : ");
+    scanf("%d", &month);
+
+    if (3 <= month && month <= 5) {
+        printf("%d月は春です。\n", month);
+    } else if (6 <= month && month <= 8) {
+        printf("%d月は夏です。\n", month);
+    } else if (9 <= month && month <= 11) {
+        printf("%d月は秋です。\n", month);
+    } else if (month == 1 || month == 2 || month == 12) {
+        printf("%d月は冬です。\n", month);
+    } else {
+        printf("%d月はありませんよ！！\n", month);
+    }
+
+    return 0;
+}
