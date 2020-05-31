9fd4b863b5bf59be8841ea9783622d588478cf99
daisaku kato <50142017+katkic@users.noreply.github.com>
2020-05-31 21:17:00 +0900

    add: list0403.c

---
+++ chap04/list0403.c
@@ -0,0 +1,26 @@
+// 整数値を次々と読み込んで合計と平均を表示
+#include <stdio.h>
+
+int main(void)
+{
+    int sum = 0;  // 合計
+    int cnt = 0;  // 整数値の個数
+    int retry;    // 処理を続けるか
+
+    do {
+        int t;
+
+        printf("整数値を入力してください : ");
+        scanf("%d", &t);
+
+        sum = sum + t;
+        cnt = cnt + 1;
+
+        printf("まだ？<Yes →  0 / No →  9>");
+        scanf("%d", &retry);
+
+    } while (retry == 0);
+
+    printf("合計は%dで平均は%.2fです。\n", sum, (double)sum / cnt);
+    return 0;
+}
