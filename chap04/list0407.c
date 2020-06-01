c8d620560c5894cc758cbd06d34c16f33f4e456e
daisaku kato <50142017+katkic@users.noreply.github.com>
2020-06-01 23:57:24 +0900

    add: list0407.c

---
+++ chap04/list0407.c
@@ -0,0 +1,19 @@
+// 読み込んだ正の整数値までカウントアップ
+
+#include <stdio.h>
+
+int main(void)
+{
+    int i, no;
+
+    printf("正の整数を入力してください : ");
+    scanf("%d", &no);
+
+    i = 0;
+    while (i <= no) {
+        printf("%d ", i++);
+    }
+    printf("\n");
+
+    return 0;
+}
