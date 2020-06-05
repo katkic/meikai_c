bb7bf836f7b0920165e491da95be18e8eae441bf
daisaku kato <50142017+katkic@users.noreply.github.com>
2020-06-05 20:28:56 +0900

    add: list0419.c

---
+++ chap04/list0419.c
@@ -0,0 +1,22 @@
+// 右下が直角の直角二等辺三角形を表示
+#include <stdio.h>
+
+int main(void)
+{
+    int i, j, len;
+
+    puts("右下直角二等辺三角形を作ります。");
+    printf("短辺 : ");
+    scanf("%d", &len);
+
+    for (i = 1; i <= len; i++) {
+        for (j = 1; j <= len - i; j++) {
+            putchar(' ');  // 空白挿入用のループ。各行にlen - i個の空白を表示する
+        }
+        for (j = 1; j <= i; j++) {
+            putchar('*');  // *表示用のループ。各行にi個の'*'を表示
+        }
+        putchar('\n');
+    }
+    return 0;
+}
