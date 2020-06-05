ca40eac120225254bf2810288e7d0754cd86bb04
daisaku kato <50142017+katkic@users.noreply.github.com>
2020-06-05 22:00:59 +0900

    add: exe0423-1.c

---
+++ chap04/exe0423-1.c
@@ -0,0 +1,20 @@
+// 演習4-23-1
+// List4-18を書きかえて、左上側が直角となる直角二等辺三角形を表示するプログラム
+#include <stdio.h>
+
+int main(void)
+{
+    int i, j, len;
+
+    puts("左上直角二等辺三角形を作ります。");
+    printf("短辺 : ");
+    scanf("%d", &len);
+
+    for (i = len; i >= 1 ; i--) {
+        for (j = 1; j <= i; j++) {
+            putchar('*');
+        }
+        putchar('\n');
+    }
+    return 0;
+}
