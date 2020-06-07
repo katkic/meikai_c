f8e2adfec0e4780e08d7ab91881a08d51d5ab96a
daisaku kato <50142017+katkic@users.noreply.github.com>
2020-06-07 12:47:24 +0900

    add: list0509.c

--- chap05/list0509.c
+++ chap05/list0509.c
@@ -5,15 +5,15 @@ int main(void)
 {
     int i;
     int score[5];
-    int max_score = 0;
+    int total_score = 0;
 
     puts("5人の点数を入力してください。");
     for (i = 0; i < 5; i++ ) {
         printf("%d番 : ", i + 1);
         scanf("%d", &score[i]);
-        max_score += score[i];
+        total_score += score[i];
     }
 
-    printf("合計点 : %5d\n", max_score);
-    printf("平均点 : %5.1f\n", (double)max_score / 5);
+    printf("合計点 : %5d\n", total_score);
+    printf("平均点 : %5.1f\n", (double)total_score / 5);
 }
