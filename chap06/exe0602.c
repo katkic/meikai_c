dc8b4a3fca50811a5e6341581f598cd30bda44fb
daisaku kato <50142017+katkic@users.noreply.github.com>
2020-06-12 00:38:05 +0900

    add: exe0602.c

---
+++ chap06/exe0602.c
@@ -0,0 +1,30 @@
+// 演習6-2
+// 三つのint型整数の最小値を返す関数を作成する
+// int min3(int a, int b, int c) {}
+
+#include <stdio.h>
+
+int min3(int a, int b, int c)
+{
+    int min = a;
+
+    if (b < min) min = b;
+    if (c < min) min = c;
+    return min;
+}
+
+int main(void)
+{
+    int n1, n2, n3;
+
+    puts("三つの整数を入力してください。");
+    printf("整数1 : ");
+    scanf("%d", &n1);
+    printf("整数2 : ");
+    scanf("%d", &n2);
+    printf("整数3 : ");
+    scanf("%d", &n3);
+
+    printf("最小値は%dです。\n", min3(n1, n2, n3));
+    return 0;
+}
