e0feb07160e7874d510e94aadf2c79dfd11ed01a
daisaku kato <50142017+katkic@users.noreply.github.com>
2020-06-13 20:57:32 +0900

    add: exe0607.c

---
+++ chap06/exe0607.c
@@ -0,0 +1,16 @@
+// 演習6-7
+// 画面に「こんにちは。」と表示する関数を作成する
+// void hello(void) {}
+
+#include <stdio.h>
+
+void hello(void)
+{
+    puts("こんにちは。");
+}
+
+int main(void)
+{
+    hello();
+    return 0;
+}
